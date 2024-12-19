7c0e6a9c0896faf50197324445bc7fb902ade3a2
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-12-19 08:54:23 +0400

    Add project files.

---
+++ HelloWorld/SimpleHelloWorld.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    public static class SimpleHelloWorld
+    {
+        public static string ReturnHelloWorld()
+        {
+            var greeting = "Hello, world!";
+            return greeting;
+        }
+    }
+}
